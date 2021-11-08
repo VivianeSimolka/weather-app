ee2d625d8354dbe10eb704542b6d1f2174c95f47
Viviane <79335849+VivianeSimolka@users.noreply.github.com>
2021-11-08 10:51:08 +0100

    Update date and time

--- sources/app.js
+++ sources/app.js
@@ -3,19 +3,43 @@ let city = "Berlin";
 let unit = "metric";
 let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
 
+function formatDateTime(timestamp) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let date = new Date(timestamp);
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
 function updateTemperature(response) {
   let temperature = document.querySelector("#current-temperature");
   let city = document.querySelector("#current-city");
   let description = document.querySelector("#description");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
+  let dateTime = document.querySelector("#date-time");
   temperature.innerHTML = Math.round(response.data.main.temp);
   city.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = response.data.main.humidity;
   wind.innerHTML = Math.round(response.data.wind.speed);
+  dateTime.innerHTML = formatDateTime(response.data.dt * 1000);
 }
-
 axios.get(apiURL).then(updateTemperature);
 
 // #humidity
