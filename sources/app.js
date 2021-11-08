86dfbdbf732e200f3b6d8ee156ecec94f437e474
Viviane <79335849+VivianeSimolka@users.noreply.github.com>
2021-11-08 10:38:30 +0100

    update current weather

--- sources/app.js
+++ sources/app.js
@@ -5,10 +5,18 @@ let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${
 
 function updateTemperature(response) {
   let temperature = document.querySelector("#current-temperature");
+  let city = document.querySelector("#current-city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
   temperature.innerHTML = Math.round(response.data.main.temp);
+  city.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
 }
 
 axios.get(apiURL).then(updateTemperature);
 
-// #current-temperature
-// #current-city
+// #humidity
+// #wind
